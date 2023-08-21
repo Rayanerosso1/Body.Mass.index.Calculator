1ab724c3b45d1b19e8c714fff425fe94e91b6ef8
rayanedasilva <rayanegdasilva@icloud.com>
2023-08-21 23:09:17 +0100

    Commit and Push

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,40 +1,53 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val altura: Float= 1.75f * 1.75f
-        val peso: Int= 83
+        val altura: Float = 1.75f * 1.75f
+        val peso: Int = 83
 
-        val result= peso / altura
+        val result = peso / altura
 
         println(result)
 
 
         val btnCalculator: Button = findViewById<Button>(R.id.bntCalculator)
-        val edtextPeso : EditText = findViewById<EditText>(R.id.edittextPeso)
-        val editAltura : EditText = findViewById<EditText>(R.id.edittextAltura)
-        val tvResult: TextView = findViewById(R.id.txtMsg)
+        val edtextPeso: EditText = findViewById<EditText>(R.id.edittextPeso)
+        val editAltura: EditText = findViewById<EditText>(R.id.edittextAltura)
+        val editname = findViewById<EditText>(R.id.edittextname)
 
         btnCalculator.setOnClickListener {
-            val altura: Float= editAltura.text.toString().toFloat()
-            val alturaFinal: Float= altura * altura
-            val peso: Float= edtextPeso.text.toString().toFloat()
-
-            val result= peso / alturaFinal
-
-            tvResult.text = result.toString()
 
 
+                val altura: Float = editAltura.text.toString().toFloat()
+                val alturaFinal: Float = altura * altura
+                val peso: Float = edtextPeso.text.toString().toFloat()
+                val name: String = editname.text.toString()
+                val result = peso / alturaFinal
+
+
+                val intent = Intent(this, ResultActivity::class.java)
+                    //aqui passamos de uma tela pra outra na Intent.
+
+                    .apply {
+
+                        putExtra("EXTRA_RESULT", result)
+                        putExtra("EXTRA_NAME", name)
+
+                    }
+                startActivity(intent)
+
+            }
         }
 
     }
-}
--- app/src/main/java/com/example/myapplication/ResultActivity.kt
+++ app/src/main/java/com/example/myapplication/ResultActivity.kt
@@ -2,10 +2,65 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+        val Tvresult = findViewById<TextView>(R.id.textview_result)
+        val Tvname = findViewById<TextView>(R.id.Tvname)
+        val result2 = intent.getStringExtra("EXTRA_NAME")
+        val result = intent.getFloatExtra("EXTRA_RESULT", 0.1f)
+        Tvresult.text = result.toString()
+
+Tvname.text= result2.toString()
+       val Tvclassification= findViewById<TextView>(R.id.textview_classificacao)
+        var classificacao = ""
+
+        if (result >= 18.5f) {
+            when (result) {
+                in 18.5f..24.9f -> {
+
+                    classificacao = "PESO NORMAL."
+
+                }
+                in 25.0f..29.9f -> {
+
+                    classificacao = "SOBREPESO 1"
+
+
+                }
+                in 30.0f..39.9f -> {
+
+                    classificacao = "OBESIDADE"
+
+                }
+                else -> {
+
+                    classificacao = "OBESIDADE GRAVE"
+
+                }
+            }
+        } else {
+
+            classificacao = "ABAIXO DO PESO."
+
+        }
+
+Tvclassification.text="Classification:$classificacao"
+
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        finish()
+        return super.onOptionsItemSelected(item)
+    }
+
 }
